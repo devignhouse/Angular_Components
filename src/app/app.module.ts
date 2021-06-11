f925010cc9f45a0f6eece532f353396041605e46
Nabiel Firfirey <devignhouse@gmail.com>
2021-06-11 13:50:44 +0200

    assignment complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// Components automatically added when GENERATING COMPONENTS using Angular CLI
+// commands below :
+// "ng g c WarningAlert"
+// or
+// "ng generate component SuccessAlert"
+
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
